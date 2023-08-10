bc1024edc9a5133dc601b83f684b9b1d357bda2d
Edu Stadler <32523518+edustadler@users.noreply.github.com>
2023-08-10 08:58:02 -0300

    .

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: 'db4free.net:3306',
-    user: 'sensedatateste',
+    host: '85.10.205.173:3306',
+    user: 'sensedata',
     password: 'sensedatateste',
     database: 'crud',
 })

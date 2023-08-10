36c89a8fdc796b002394a4a03c03f0c4354e4c5f
Edu Stadler <32523518+edustadler@users.noreply.github.com>
2023-08-10 09:47:22 -0300

    gone

--- database.js
+++ database.js
@@ -1,10 +1,10 @@
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: 'db4free.net',
+    host: 'sql10.freemysqlhosting.net',
     port: 3306,
-    user: 'sensedatateste',
-    password: 'sensedatateste',
+    user: 'sql10638895',
+    password: 'uk2SPVdHgM',
     database: 'crud',
     debug: true
 })

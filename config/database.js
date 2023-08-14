9eac564cd08e1c45027b41448cf8ca1e4daec754
Edu Stadler <32523518+edustadler@users.noreply.github.com>
2023-08-14 06:24:12 -0300

    hide mongo login

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-const dbConfig = "mongodb+srv://vercel-admin-user:dGNsY1KOvXp0E6gm@cluster0.7cxwuec.mongodb.net/financialdata?retryWrites=true&w=majority";
+
 
 
 export const connection = mongoose.connect(dbConfig, {
